e2f164be926e8498e4c0d2deb45f9110aa0f0f62
poudel123 <42462463+poudel123@users.noreply.github.com>
2018-10-12 17:48:40 +0530

    Add files via upload

---
+++ microservicedemo/src/main/java/com/configuration/microservicedemo/MicroservicedemoApplication.java
@@ -0,0 +1,13 @@
+package com.configuration.microservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
+@SpringBootApplication
+public class MicroservicedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicedemoApplication.class, args);
+	}
+}
